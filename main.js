e5978006395d3614316e282b6ad6499a1f9a7c20
Valerio Valletta <valerio.valletta@icloud.com>
2025-01-04 19:09:47 +0100

    add cookie to keep track of last box lyric position

--- main.js
+++ main.js
@@ -9,12 +9,39 @@
 // @grant        none
 // ==/UserScript==
 
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
 function dragElement(elmnt) {
   var pos1 = 0,
     pos2 = 0,
     pos3 = 0,
     pos4 = 0;
-  console.log(document.getElementById(elmnt.id + "-header"));
+
+  if (getCookie("lyrics-box-pos")) {
+    const { top, left } = JSON.parse(getCookie("lyrics-box-pos"));
+
+    elmnt.style.top = top;
+    elmnt.style.left = left;
+  }
+
   if (document.getElementById(elmnt.id + "-header")) {
     // if present, the header is where you move the DIV from:
     document.getElementById(elmnt.id + "-header").onmousedown = dragMouseDown;
@@ -46,6 +73,13 @@ function dragElement(elmnt) {
 
   function closeDragElement() {
     // stop moving when mouse button is released:
+    const lyricsBox = document.querySelector("#lyrics-box");
+    const lastBoxPos = {
+      top: lyricsBox.style.top,
+      left: lyricsBox.style.left,
+    };
+
+    setCookie("lyrics-box-pos", JSON.stringify(lastBoxPos));
     document.onmouseup = null;
     document.onmousemove = null;
   }
